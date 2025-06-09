22cba6daf60760f416b9fab75d5ff380dfac8b91
wkwtfigo <e.zagurskih@innopolis.university>
2025-06-09 21:50:27 +0300

    <back> fix: [TS-16] initialize working backend

--- back/src/main/java/TeamSync/example/TeamSync/TeamSyncApplication.java
+++ back/src/main/java/TeamSync/example/TeamSync/TeamSyncApplication.java
@@ -1,9 +1,16 @@
 package TeamSync.example.TeamSync;
 
+import org.springframework.ai.vectorstore.redis.autoconfigure.RedisVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    RedisVectorStoreAutoConfiguration.class
+})
 public class TeamSyncApplication {
 
 	public static void main(String[] args) {
