687f862cb6cba02b63114960e779243b14a12dc7
aequor-cntrl <53840534+aequor-cntrl@users.noreply.github.com>
2025-06-17 17:07:38 +0300

    <front> feature: [TS-29] add skeleton pages

---
+++ front/src/pages/splashScreen.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import SplashHeader from '../components/splashHeader'
+import Footer from '../components/footer';
+export default function SplashScreen() {
+  return(
+    <div className='flex flex-col justify-between h-screen'>
+      <SplashHeader/>
+      <div className='lg:flex lg:flex-row lg:justify-between lg:items-center mr-5 ml-5 lg:mr-18 lg:ml-18'>
+        <div className='flex flex-col justify-center items-center gap-5 lg:w-[60%]'>
+          <h1 className='text-(--secondary-color) font-[Manrope] font-extrabold lg:text-7xl text-3xl'>
+            Build your dream team for every project.
+          </h1>
+          <h2 className='text-(--accent-color-1) font-[Manrope] text-md lg:text-2xl'>
+            TeamSync uses structured data and AI recommendations to help students form balanced, 
+            high-performing teams — reducing overhead for faculty and improving outcomes.
+          </h2>
+        </div>
+        <img className = 'hidden lg:block pr-10 w-[30%] h-auto' src='./splashGraphic.jpg' 
+        alt="Two people looking at each other from open windows drawn in a minimalist style" />
+      </div>
+      <Footer />
+    </div>
+  );
+}
