f5899f964a02d7400afc8357c81236b075347f5e
Stepan Dementev <dementevssstepan@gmail.com>
2025-06-18 19:10:58 +0300

    feature: [TS-64] init spring template

---
+++ back/projects/src/test/java/ru/teamsync/projects/ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teamsync.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
