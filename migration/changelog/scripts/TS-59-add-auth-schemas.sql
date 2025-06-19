f119687cbfdbd388f9139679703024c813c058d0
Stepan Dementev <dementevssstepan@gmail.com>
2025-06-19 18:47:29 +0300

    feature: [TS-59] add migrations

--- migration/changelog/scripts/TS-59-add-auth-schemas.sql
+++ migration/changelog/scripts/TS-59-add-auth-schemas.sql
@@ -1,6 +1,6 @@
 CREATE TABLE internal_external_user_id(
     internal_user_id INTEGER,
     external_user_id VARCHAR(64),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (internal_user_id, external_user_id)
 )
