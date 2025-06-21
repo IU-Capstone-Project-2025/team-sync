b2f5ec6c0717106d26aaa5c757ed5ce87e645af5
Danis Sharafiev <harnemer@gmail.com>
2025-06-21 20:10:09 +0300

    <ml> feature: [TS-55] add connection with DB
    * feature: [TS-55] create basic DB-conn logic
    
    * feature: [TS-55] create logger and config data
    
    * feature: [TS-55] create lifespan fuction for db connection
    
    * feature: [TS-55] update requirements.txt
    
    * fix: [TS-55] add correct db closing
    
    * chore: [TS-55] refactor database model
    
    * fix: [TS-55] fix async error
    
    * fix: [TS-55] removed SQL injection
    
    * chore [TS-55] delete unnecessary library
    
    * feature: [TS-55] add item skills getting

--- ml/recsys-service/config/config.py
+++ ml/recsys-service/config/config.py
@@ -1,2 +1,8 @@
 class Config:
-    NUM = 1
+    POSTGRES_HOST = "postgres"
+    POSTGRES_USER = "dev-user"
+    POSTGRES_PASSWORD = "dev-psw"
+    POSTGRES_DB = "dev-db"
+    POSTGRES_PORT = 5432
+    DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
--- ml/recsys-service/main.py
+++ ml/recsys-service/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from api.endpoints.v1.initial_router import router
+from database.base_model import DBModel
+from config.logging import setup_logging
 
-app = FastAPI()
+async def lifespan(app: FastAPI):    
+    app.state.logger = setup_logging()
+    logger = app.state.logger
+    logger.info("Application startup")
+    app.state.db = DBModel(logger=logger)
+    await app.state.db.connect()
+    logger.info("Database connection established successfully.")
+    yield
+    logger.info("Application shutdown")
+    if app.state.db and app.state.db.connection:
+        logger.info("Closing database connection...")
+        await app.state.db.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=router)
