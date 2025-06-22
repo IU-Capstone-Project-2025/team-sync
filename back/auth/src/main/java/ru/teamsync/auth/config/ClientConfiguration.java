f402214d5acff931758723c74d67f06f319c55f5
Stepan Dementev <dementevssstepan@gmail.com>
2025-06-22 23:20:16 +0300

    feature: [TS-59] add client config

---
+++ back/auth/src/main/java/ru/teamsync/auth/config/ClientConfiguration.java
@@ -0,0 +1,20 @@
+package ru.teamsync.auth.config;
+
+import io.github.danielliu1123.httpexchange.HttpClientCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.teamsync.auth.config.properties.ClientProperties;
+
+@Configuration
+@EnableConfigurationProperties(ClientProperties.class)
+public class ClientConfiguration {
+
+    @Bean
+    public HttpClientCustomizer.RestClientCustomizer restClientCustomizer(ClientProperties.ResumeServiceClientProperties clientProperties) {
+        return (client, channel) -> {
+            client.baseUrl(clientProperties.apiUrl());
+        };
+    }
+
+}
