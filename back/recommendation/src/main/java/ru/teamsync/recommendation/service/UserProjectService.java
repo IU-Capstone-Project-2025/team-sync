da12da9fee57a014c6fb3e7fe607e32d77013f87
Stepan Dementev <dementevssstepan@gmail.com>
2025-06-24 20:58:54 +0300

    feature: [TS-86] add redis service

--- back/recommendation/src/main/java/ru/teamsync/recommendation/service/UserProjectService.java
+++ back/recommendation/src/main/java/ru/teamsync/recommendation/service/UserProjectService.java
@@ -1,5 +1,6 @@
 package ru.teamsync.recommendation.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -9,31 +10,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Log4j2
 public class UserProjectService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final ListOperations<String, Object> listOps;
 
     public UserProjectService(RedisTemplate<String, Object> redisTemplate) {
-        this.redisTemplate = redisTemplate;
         this.listOps = redisTemplate.opsForList();
     }
 
-    public void addProjectScore(String userId, ProjectScore projectScore) {
-        String key = "user:" + userId + ":projects";
-        listOps.rightPush(key, projectScore);
-    }
-
     public List<ProjectScore> getProjectScores(int userId) {
-        String key = "user:" + userId + ":projects";
-        List<Object> rawList = listOps.range(key, 0, -1);
+        return List.of();
 
-        return rawList.stream()
-                .map(obj -> (ProjectScore) obj)
-                .collect(Collectors.toList());
+//        String key = String.valueOf(userId);
+//        log.info("Fetching project scores for user: {}", key);
+//        List<Object> rawList = listOps.range(key, 0, -1);
+//
+//        return rawList.stream()
+//                .map(obj -> (ProjectScore) obj)
+//                .collect(Collectors.toList());
     }
 
-    public void deleteUserProjects(String userId) {
-        redisTemplate.delete("user:" + userId + ":projects");
-    }
 }
--- back/recommendation/src/main/java/ru/teamsync/recommendation/service/grpc/ProjectsRecommendationServiceGrpcImpl.java
+++ back/recommendation/src/main/java/ru/teamsync/recommendation/service/grpc/ProjectsRecommendationServiceGrpcImpl.java
@@ -2,14 +2,17 @@ package ru.teamsync.recommendation.service.grpc;
 
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import net.devh.boot.grpc.server.service.GrpcService;
 import ru.teamsync.grpc.ProjectsRecommendationsGrpc;
 import ru.teamsync.grpc.RecommendationProto;
 import ru.teamsync.grpc.RecommendationProto.ProjectsRecommendationsRequest;
 import ru.teamsync.grpc.RecommendationProto.ProjectsRecommendationsResponse;
 import ru.teamsync.recommendation.service.UserProjectService;
 
-@Service
+@GrpcService
+@Log4j2
 @RequiredArgsConstructor
 public class ProjectsRecommendationServiceGrpcImpl extends ProjectsRecommendationsGrpc.ProjectsRecommendationsImplBase {
 
@@ -17,6 +20,8 @@ public class ProjectsRecommendationServiceGrpcImpl extends ProjectsRecommendatio
 
     @Override
     public void fetchRecommendation(ProjectsRecommendationsRequest request, StreamObserver<ProjectsRecommendationsResponse> responseObserver) {
+        log.info("Got fetchRecommendation request: {}", request);
+
         int userId = request.getUserId();
         ProjectsRecommendationsResponse.Builder responseBuilder = ProjectsRecommendationsResponse.newBuilder();
 
