1ff427e2f8040281c1c18beae2470aecaf952149
Danis Sharafiev <harnemer@gmail.com>
2025-06-25 11:14:42 +0300

    fix: [TS-73] fix permissions issue

--- airflow/dags/curl_recommendations_dag.py
+++ airflow/dags/curl_recommendations_dag.py
@@ -3,15 +3,15 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
-    "start_date": datetime(2024, 6, 24),  # Обновите дату
+    "start_date": datetime(2024, 6, 24),
 }
 
 with DAG(
     dag_id="curl_recommendations",
     default_args=default_args,
-    schedule="* * * * *",
+    schedule="*/5 * * * *",
     catchup=False,
-    is_paused_upon_creation=False,  # DAG будет активен сразу
+    is_paused_upon_creation=False,
 ) as dag:
     curl_task = BashOperator(
         task_id="post_recommendations",
