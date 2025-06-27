359537ba263d46478cb5bc718ae525f4bca02a60
wkwtfigo <e.zagurskih@innopolis.university>
2025-06-27 23:45:41 +0300

    feature: [TS-113] add logbook configuration for all backend services

---
+++ back/auth/src/main/java/ru/teamsync/auth/config/LogBookConfig.java
@@ -0,0 +1,26 @@
+package ru.teamsync.auth.config;
+
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.DefaultCorrelationId;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.core.DefaultStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogBookConfig {
+
+    @Bean
+    public Logbook logbook() {
+        return Logbook.builder()
+                .sink(new DefaultSink(
+                        new DefaultHttpLogFormatter(),
+                        new DefaultHttpLogWriter()
+                ))
+                .strategy(new DefaultStrategy())
+                .correlationId(new DefaultCorrelationId())
+                .build();
+    }
+}
--- back/projects/src/main/java/ru/teamsync/projects/ProjectsApplication.java
+++ back/projects/src/main/java/ru/teamsync/projects/ProjectsApplication.java
@@ -1,8 +1,11 @@
 package ru.teamsync.projects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 import ru.teamsync.projects.config.properties.JwtProperties;
 
 @SpringBootApplication
@@ -10,8 +13,10 @@ import ru.teamsync.projects.config.properties.JwtProperties;
         JwtProperties.class
 })
 public class ProjectsApplication {
+    private static final Logger log = LoggerFactory.getLogger(ProjectsApplication.class);
 
     public static void main(String[] args) {
+        log.info("Start of backend-projects");
         SpringApplication.run(ProjectsApplication.class, args);
     }
 
