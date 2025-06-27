d7418bce899a91359af1eef95fda4bfa9808b35d
wkwtfigo <e.zagurskih@innopolis.university>
2025-06-27 12:31:49 +0300

    feature: [TS-113] add initial version of logging

---
+++ back/resume/src/main/java/ru/teamsync/resume/config/LogBookConfiguration.java
@@ -0,0 +1,23 @@
+package ru.teamsync.resume.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.DefaultSink;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.DefaultHttpLogWriter;
+
+@Configuration
+public class LogBookConfiguration {
+
+    @Bean
+    public Logbook logbook() {
+        return Logbook.builder()
+                .sink(new DefaultSink(
+                    new JsonHttpLogFormatter(),
+                    new DefaultHttpLogWriter()
+                ))
+                .build();
+    }
+}
