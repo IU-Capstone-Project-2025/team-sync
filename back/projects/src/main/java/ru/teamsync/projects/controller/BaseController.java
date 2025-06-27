b0f194f01d65a18a33ead905ec9d20952409cbeb
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2025-06-27 15:44:59 +0300

    fix: [TS-118] fix swagger 500 in projects
    * build: [TS-118] raise open api dependency version
    
    * fix: [TS-118] update jdbc url in projects
    
    * feature: [TS-118] add ml service
    
    ---------
    
    Co-authored-by: Stepan Dementev <dementevssstepan@gmail.com>

--- back/projects/src/main/java/ru/teamsync/projects/controller/BaseController.java
+++ back/projects/src/main/java/ru/teamsync/projects/controller/BaseController.java
@@ -1,5 +1,6 @@
 package ru.teamsync.projects.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,16 +9,19 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.teamsync.projects.dto.response.BaseResponse;
 
 @RestControllerAdvice
+@Log4j2
 public class BaseController {
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<BaseResponse<Void>> handleNotFound(NoResourceFoundException ex) {
+        log.info(ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(BaseResponse.withErrorMessage(ex.getResourcePath()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<BaseResponse<Void>> handleAny(NoResourceFoundException ex) {
+    public ResponseEntity<BaseResponse<Void>> handleAny(Exception ex) {
+        log.error(ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(BaseResponse.withErrorMessage(ex.getMessage()));
     }
