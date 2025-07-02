b39f6558fcb00d4144fe17530c0ad44c8fc8d9ab
Stepan Dementev <dementevssstepan@gmail.com>
2025-07-02 16:40:34 +0300

    fix: [TS-173] handle resume client exceptions

--- back/auth/src/main/java/ru/teamsync/auth/controllers/BaseController.java
+++ back/auth/src/main/java/ru/teamsync/auth/controllers/BaseController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+import ru.teamsync.auth.client.ResumeClientException;
 import ru.teamsync.auth.controllers.response.BaseResponse;
 import ru.teamsync.auth.services.AuthConflictException;
 
@@ -20,6 +21,12 @@ public class BaseController {
     @ExceptionHandler(AuthConflictException.class)
     public ResponseEntity<BaseResponse<Void>> handleUserNotRegistered(AuthConflictException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(BaseResponse.withErrorMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResumeClientException.class)
+    public ResponseEntity<BaseResponse<Void>> handleUserNotRegistered(ResumeClientException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(BaseResponse.withErrorMessage(ex.getMessage()));
     }
 }
