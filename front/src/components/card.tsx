84e77fb594c353d27ca7a7e8fd8552443874388e
aequor-cntrl <53840534+aequor-cntrl@users.noreply.github.com>
2025-07-02 18:46:03 +0300

    <front> fix: [TS-174] fix autocompletion

--- front/src/components/card.tsx
+++ front/src/components/card.tsx
@@ -9,7 +9,7 @@ function truncateString({string, maxLength} : {string: string, maxLength: number
 
 export default function Card({props}) {
   return (
-    <div className="mt-4 bg-(--header-footer-color) w-full max-w-[98vw] text-(--secondary-color) p-5 border-(--secondary-color) border-1 rounded-2xl">
+    <div className="mt-4 bg-(--header-footer-color) w-full max-w-[98vw] text-(--secondary-color) p-5 border-(--secondary-color) border-1 rounded-2xl hover:shadow-2xl">
       <h1 className="pb-4 font-[Manrope] font-extrabold text-4xl">{props.projName}</h1>
       <p className="pb-4 font-[Inter] text-xl max-w-[70%]">{truncateString({ string: props.description, maxLength: 350 })}</p>
       <p className="font-[Inter] text-lg"><em className="font-bold">Available roles:</em> {props.roles.join(", ")}</p>
