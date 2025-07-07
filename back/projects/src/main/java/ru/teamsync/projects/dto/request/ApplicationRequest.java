26cebd80ce3c9b8c4d6cc7a06af0c5bb2c21c55c
wkwtfigo <e.zagurskih@innopolis.university>
2025-07-08 00:24:34 +0300

    fix: [TS-196]fix annotations

--- back/projects/src/main/java/ru/teamsync/projects/dto/request/ApplicationRequest.java
+++ back/projects/src/main/java/ru/teamsync/projects/dto/request/ApplicationRequest.java
@@ -1,7 +1,11 @@
 package ru.teamsync.projects.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotNull;
 
-public record ApplicationRequest(@NotNull Long projectId) {
-
-}
+public record ApplicationRequest(
+    @NotNull 
+    @JsonProperty("projectId")
+    Long projectId 
+) {}
