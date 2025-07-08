d1d51b4bffad6608684f08ec86f9649897880221
Stepan Dementev <dementevssstepan@gmail.com>
2025-07-08 22:25:40 +0300

    feature: [TS-207] update resume service client

--- back/auth/src/main/java/ru/teamsync/auth/client/dto/student/StudentCreationResponse.java
+++ back/auth/src/main/java/ru/teamsync/auth/client/dto/student/StudentCreationResponse.java
@@ -1,7 +1,12 @@
 package ru.teamsync.auth.client.dto.student;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record StudentCreationResponse(
+        @JsonProperty("student_id")
         Integer studentId,
+
+        @JsonProperty("person_id")
         Integer personId
 ) {
 }
