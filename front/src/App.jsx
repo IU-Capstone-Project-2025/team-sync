9f7428057d177232bdfcf72ef6262c823ccd2a4f
aequor <53840534+aequor-cntrl@users.noreply.github.com>
2025-07-09 18:55:23 +0300

    <front> feature: [TS-198] create responses page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,13 +4,15 @@ import SplashScreen from './pages/splashScreen'
 import HomeScreen from './pages/homeScreen'
 import ProjectScreen from "./pages/projectScreen"
 import CreateProjectScreen from './pages/createProjectScreen';
+import ResponseScreen from './pages/responsesScreen';
 function App() {
   return (
     <Routes>
       <Route path = "/" element = {<SplashScreen />} />
       <Route path = "/home" element = {<HomeScreen />} />
       <Route path = "/projects" element = {<ProjectScreen />} />
-      <Route path = "/create_project" element = {<CreateProjectScreen/>} />
+      <Route path = "/projects/create" element = {<CreateProjectScreen/>} />
+      <Route path = "/responses" element = {<ResponseScreen/>} />
     </Routes>
   )
 }
