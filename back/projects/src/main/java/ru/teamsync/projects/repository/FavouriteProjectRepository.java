b45cbf2646a2f6b5f8e68d158576eb7a7098a729
Stepan Dementev <dementevssstepan@gmail.com>
2025-07-10 21:13:07 +0300

    fix: [TS-212] update fav project deletion

--- back/projects/src/main/java/ru/teamsync/projects/repository/FavouriteProjectRepository.java
+++ back/projects/src/main/java/ru/teamsync/projects/repository/FavouriteProjectRepository.java
@@ -13,10 +13,9 @@ import ru.teamsync.projects.entity.Project;
 public interface FavouriteProjectRepository extends JpaRepository<FavouriteProject, Long> {
     Page<FavouriteProject> findAllByPersonId(Long personId, Pageable pageable);
     boolean existsByProjectIdAndPersonId(Long projectId, Long personId);
-    void deleteByProjectIdAndPersonId(Long projectId, Long personId);
 
     @Modifying
     @Transactional
-    @Query("DELETE FROM FavouriteProject fp WHERE fp.project.id = :projectId")
-    void deleteByProjectId(Long projectId);
+    @Query("DELETE FROM FavouriteProject fp WHERE fp.personId = :personId AND fp.project.id = :projectId")
+    void deleteByPersonIdAndProjectId(Long personId, Long projectId);
 }
