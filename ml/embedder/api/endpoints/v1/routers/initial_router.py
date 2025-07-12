7df08256003029c6e97c57f3029fc43e9ee82b68
Danis Sharafiev <harnemer@gmail.com>
2025-07-12 19:44:57 +0300

    feature: [TS-229] add similarities for different collections

--- ml/embedder/api/endpoints/v1/routers/initial_router.py
+++ ml/embedder/api/endpoints/v1/routers/initial_router.py
@@ -20,7 +20,7 @@ async def create_points(req: Request, request: QdrantEmbeddingRequest):
     text = req.app.state.postgres.fetch_description(request.table, request.id)
     embeddings = req.app.state.onnx.get_embeddings(text)
     embeddings = [emb.tolist() if hasattr(emb, 'tolist') else emb for emb in embeddings]
-    req.app.state.qdrant.add_embeddings(request.table, request.id, embeddings)
+    req.app.state.qdrant.update_embeddings(request.table, request.id, embeddings)
     return {"status": "points created"}
 
 @router.get("/points/{table}/{id}")
@@ -29,3 +29,17 @@ async def get_points(req: Request, table: str, id: int):
         return {"error": "Invalid table name. Use 'project' or 'student'."}
     points = req.app.state.qdrant.get_embeddings(table, id)
     return {"points": points}
+
+@router.get("/similarities/{table}/{init_id}")
+async def get_similarities(req: Request, table: str, init_id: int):
+    if table not in ["project", "student"]:
+        return {"error": "Invalid table name. Use 'project' or 'student'."}
+    similarities = req.app.state.qdrant.get_similarities(table, init_id)
+    return {"similarities": similarities}
+
+@router.get("/similarities/{table}/{init_id}/among_projects")
+async def get_similarities_among_projects(req: Request, table: str, init_id: int):
+    if table not in ["project", "student"]:
+        return {"error": "Invalid table name. Use 'project' or 'student'."}
+    similarities = req.app.state.qdrant.get_similarities(table, init_id, among_projects=True)
+    return {"similarities": similarities}
--- ml/embedder/models/qdrant_model.py
+++ ml/embedder/models/qdrant_model.py
@@ -17,7 +17,7 @@ class QdrantModel:
         else:
             self.logger.info(f"Collection '{collection_name}' already exists, skipping creation.")
 
-    def add_embeddings(self, collection_name, ids, embeddings):
+    def update_embeddings(self, collection_name, ids, embeddings):
         if not isinstance(ids, list):
             ids = [ids]
         if not isinstance(embeddings, list):
@@ -33,3 +33,27 @@ class QdrantModel:
         if not isinstance(ids, list):
             ids = [ids]
         return self.client.retrieve(collection_name, ids, with_vectors=True)
+
+    def get_similarities(self, collection_name: str, init_id: int, among_projects = False):
+        collection_name_2 = collection_name
+        if among_projects:
+            collection_name_2 = "project"
+        info = self.client.get_collection(collection_name)
+        if info.points_count == 0:
+            return []
+        point = self.client.retrieve(collection_name, [init_id], with_vectors=True)
+        if not point or not hasattr(point[0], 'vector') or point[0].vector is None:
+            return []
+        info_2 = self.client.get_collection(collection_name_2)
+        if info_2.points_count == 0:
+            return []
+        query_vector = point[0].vector
+        result = self.client.search(
+            collection_name=collection_name_2,
+            query_vector=query_vector,
+            limit=info_2.points_count
+        )
+        result = [{"table": collection_name_2, "id": res.id, "score": res.score} for res in result]
+        return result
+    
+    
