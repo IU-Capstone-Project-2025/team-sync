edfe82c8ac0211c6286c7d76a158586181787ab6
Danis Sharafiev <harnemer@gmail.com>
2025-07-13 14:25:21 +0300

    fix: [TS-230] rename table properly

--- migration/changelog/scripts/TS-230-create-clicks-table.sql
+++ migration/changelog/scripts/TS-230-create-clicks-table.sql
@@ -1,6 +1,6 @@
-CREATE TABLE user_project_clicks (
-    user_id      INT NOT NULL REFERENCES student(id),
-    project_id   INT NOT NULL REFERENCES project(id),
-    clicked      INT NOT NULL DEFAULT 1,
+CREATE TABLE user_project_click (
+    user_id INT NOT NULL REFERENCES student(id),
+    project_id INT NOT NULL REFERENCES project(id),
+    click_count INT NOT NULL DEFAULT 1,
     PRIMARY KEY (user_id, project_id)
 );
