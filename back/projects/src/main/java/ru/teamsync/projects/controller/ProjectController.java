ffffdd78c71ed99f32a4b6004ce1467e3e87b24d
wkwtfigo <e.zagurskih@innopolis.university>
2025-07-14 21:45:59 +0300

    back/fix/TS-246-fix-typo

--- back/projects/src/main/java/ru/teamsync/projects/controller/ProjectController.java
+++ back/projects/src/main/java/ru/teamsync/projects/controller/ProjectController.java
@@ -3,15 +3,24 @@ package ru.teamsync.projects.controller;
 import java.nio.file.AccessDeniedException;
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import ru.teamsync.projects.dto.request.ProjectCreateRequest;
 import ru.teamsync.projects.dto.request.ProjectUpdateRequest;
 import ru.teamsync.projects.dto.request.UpdateApplicationStatusRequest;
@@ -47,7 +56,7 @@ public class ProjectController {
         return BaseResponse.of(myProjects);
     }
 
-    @GetMapping("{projectId}")
+    @GetMapping("/{projectId}")
     public BaseResponse<ProjectResponse> getProjectById(
             @PathVariable Long projectId) {
             
