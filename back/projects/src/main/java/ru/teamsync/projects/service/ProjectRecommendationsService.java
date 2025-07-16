5504b27236a238976dc8a24b4281c20f948decfe
Stepan Dementev <dementevssstepan@gmail.com>
2025-07-16 17:04:13 +0300

    feature: [TS-104] add project recommendation service

--- back/projects/src/main/java/ru/teamsync/projects/service/ProjectRecommendationsService.java
+++ back/projects/src/main/java/ru/teamsync/projects/service/ProjectRecommendationsService.java
@@ -1,14 +1,46 @@
 package ru.teamsync.projects.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ru.teamsync.grpc.ProjectsRecommendationsGrpc;
+import ru.teamsync.grpc.RecommendationProto;
+import ru.teamsync.projects.dto.response.ProjectResponse;
 import ru.teamsync.projects.entity.Project;
+import ru.teamsync.projects.mapper.ProjectMapper;
+import ru.teamsync.projects.repository.ProjectRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectRecommendationsService {
 
-    public Page<Project> getProjectRecommendations(long userId){
+    private final ProjectsRecommendationsGrpc.ProjectsRecommendationsBlockingStub projectsRecommendationsStub;
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
 
+    public List<ProjectResponse> getProjectRecommendationsForUser(int userId, Pageable pageable) {
+        int readStart = pageable.getPageSize() * pageable.getPageNumber();
+        int amount = pageable.getPageSize();
+        var request = RecommendationProto.ProjectsRecommendationsRequest
+                .newBuilder()
+                .setUserId(userId)
+                .setReadStart(readStart)
+                .setAmount(amount)
+                .build();
+
+        var projectIds = projectsRecommendationsStub
+                .fetchRecommendation(request)
+                .getProjectScoresList()
+                .stream()
+                .map(RecommendationProto.ProjectScore::getProjectId)
+                .map(Integer::longValue)
+                .toList();
+
+        var projects = projectRepository.findAllById(projectIds);
+
+        return projects.stream().map(projectMapper::toDto).toList();
     }
-
 }
