d2ea09d8d4abb7d924a3ca59b1ee169359f4e109
Kamilya Shakirova <94891282+Kamilya05@users.noreply.github.com>
2025-07-17 05:58:12 +0300

    <ml> feature: [TS-268] enhance role-based

--- ml/recsys-service/models/role_based.py
+++ ml/recsys-service/models/role_based.py
@@ -4,6 +4,37 @@ from models.base_recommender import Recommender
 from config.config import Config
 
 
+def get_faiss_recommendations(num_roles, project_ids, projects_with_roles_v, user_roles_v):
+    index = faiss.IndexFlatL2(num_roles)
+    index.add(projects_with_roles_v)
+
+    distances, indices = index.search(user_roles_v, len(project_ids))
+    distances, indices = distances[0], indices[0]
+    
+    min_dist, max_dist = np.min(distances), np.max(distances)
+    normalized_scores = 1 - (distances - min_dist) / (max_dist - min_dist)
+
+    recommendations = []
+    for i, score in zip(indices, normalized_scores):
+        recommendations.append((project_ids[i], score))
+    recommendations = [i[1] for i in sorted(recommendations, key=lambda x: x[0])]
+    return recommendations
+
+
+def get_IoU_recomendations(user_roles, projects_with_roles):
+    recommendations = []
+    for project_roles in projects_with_roles:
+        recommendations.append(len(np.intersect1d(user_roles, project_roles)) / len(np.union1d(user_roles, project_roles)))
+    return recommendations
+
+
+def get_OL_recomendations(user_roles, projects_with_roles):
+    recommendations = []
+    for project_roles in projects_with_roles:
+        recommendations.append(len(np.intersect1d(user_roles, project_roles)) / min(len(user_roles), len(project_roles)))
+    return recommendations
+
+
 class RoleBasedRecommender(Recommender):
     def __init__(self, DBModel, logger, model_name):
         super().__init__(DBModel, logger, model_name)
@@ -11,6 +42,7 @@ class RoleBasedRecommender(Recommender):
         self.logger.info(f"Initialized {self.model_name} role-based recommender.")
         self.all_roles = {}
         self.projects_with_roles = []
+        self.projects_with_roles_v = []
         
     def calculate_scores(self, user_id, project_ids=None):
         """Get project recommendations based on project roles."""
@@ -25,22 +57,15 @@ class RoleBasedRecommender(Recommender):
         for role in self.db.get_user_roles(user_id):
             user_roles_v[0][self.all_roles[role]] = 1
 
-        index = faiss.IndexFlatL2(num_roles)
-        index.add(self.projects_with_roles)
-        distances, indices = index.search(user_roles_v, num_projects)
-        distances, indices = distances[0], indices[0]
-
-        min_dist, max_dist = np.min(distances), np.max(distances)
-        if max_dist == min_dist:
-            normalized_scores = np.ones_like(distances)  # Assign uniform scores
-        else:
-            normalized_scores = 1 - (distances - min_dist) / (max_dist - min_dist)
+        recommendations_L2 = get_faiss_recommendations(num_roles, project_ids, self.projects_with_roles_v, user_roles_v)
+        recommendations_IoU = get_IoU_recomendations(self.db.get_user_roles(user_id), self.projects_with_roles)
+        recommendations_OL = get_OL_recomendations(self.db.get_user_roles(user_id), self.projects_with_roles)
 
         recommendations = []
-        for i, score in zip(indices, normalized_scores):
-            recommendations.append((project_ids[i], score))
-        recommendations = [i[1] for i in sorted(recommendations, key=lambda x: x[0])]
-
+        for score_id in range(num_projects):
+            recommendations.append(recommendations_L2[score_id] * self.config.ROLE_L2_COEFFICIENT +
+                                   recommendations_IoU[score_id] * self.config.ROLE_IOU_COEFFICIENT +
+                                   recommendations_OL[score_id] * self.config.ROLE_OL_COEFFICIENT)
         return recommendations  # [0.14, 0.1, 0.2, 0.15]
     
     def save_data_for_calculation(self, project_ids=None, user_ids=None):
@@ -55,8 +80,10 @@ class RoleBasedRecommender(Recommender):
         num_roles = len(self.all_roles)
         num_projects = len(project_ids)
 
-        self.projects_with_roles = np.zeros(shape=(num_projects, num_roles), dtype=np.float32)
+        self.projects_with_roles_v = np.zeros(shape=(num_projects, num_roles), dtype=np.float32)
         for i in range(num_projects):
+            self.projects_with_roles.append([])
             for role in self.db.get_project_roles(project_ids[i]):
-                self.projects_with_roles[i][self.all_roles[role]] = 1
+                self.projects_with_roles[-1].append(role)
+                self.projects_with_roles_v[i][self.all_roles[role]] = 1
         
