6573cd0c5d63d5a5cae603878968b4644ff716b3
Stepan Dementev <dementevssstepan@gmail.com>
2025-07-17 21:49:36 +0300

    fix: [TS-104] fix recommendation service

--- back/recommendation/src/main/java/ru/teamsync/recommendation/service/UserProjectService.java
+++ back/recommendation/src/main/java/ru/teamsync/recommendation/service/UserProjectService.java
@@ -1,35 +1,23 @@
 package ru.teamsync.recommendation.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import ru.teamsync.recommendation.model.ProjectScore;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class UserProjectService {
 
-    private final ListOperations<String, Object> listOps;
-    private final ObjectMapper objectMapper;
+    private final ListOperations<String, ProjectScore> listOps;
 
-    public UserProjectService(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper) {
-        this.listOps = redisTemplate.opsForList();
-        this.objectMapper = objectMapper;
-    }
-
-    public List<ProjectScore> getProjectScores(int userId) {
+    public List<ProjectScore> getProjectScores(long userId, long start, long end) {
         String key = String.valueOf(userId);
-        log.info("Fetching project scores for user: {}", key);
-        List<Object> rawList = listOps.range(key, 0, -1);
-
-        return rawList.stream()
-                .map(obj -> objectMapper.convertValue(obj, ProjectScore.class))
-                .collect(Collectors.toList());
+        return listOps.range(key, start, end-1);
     }
 
 }
