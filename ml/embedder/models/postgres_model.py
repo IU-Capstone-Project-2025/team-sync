03d687f46a8d7b99694e5d3422a5ffba476ee185
Danis Sharafiev <harnemer@gmail.com>
2025-07-18 18:52:59 +0300

    feature: [TS-278] add async for methods

--- ml/embedder/models/postgres_model.py
+++ ml/embedder/models/postgres_model.py
@@ -3,8 +3,8 @@ from psycopg2 import sql
 from config.config import Config
 
 class PostgresModel:
-    def __init__(self, host: str = Config.POSTGRES_HOST, 
-                 port: int = Config.POSTGRES_PORT, 
+    def __init__(self, host: str = Config().POSTGRES_HOST, 
+                 port: int = Config().POSTGRES_PORT, 
                  user: str = Config().POSTGRES_USER, 
                  password: str = Config().POSTGRES_PASSWORD, 
                  dbname: str = Config().POSTGRES_DBNAME):
